55ddc8795e6576aedcd9c976cd4f6b801332bb07
GabrielleHandy <gehandy123@gmail.com>
2023-12-24 21:42:11 -0500

    Create TravelPlanner Repository

---
+++ Java Update/src/main/java/com/example/travelbuddy_api/repository/TravelPlannerRepository.java
@@ -0,0 +1,9 @@
+package com.example.travelbuddy_api.repository;
+
+import com.example.travelbuddy_api.model.TravelPlanner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelPlannerRepository extends JpaRepository<TravelPlanner, Long> {
+
+
+}
