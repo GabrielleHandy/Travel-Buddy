703db5eca3d3b0c6353347bdab509f2f74def8da
GabrielleHandy <gehandy123@gmail.com>
2023-12-24 21:51:20 -0500

    Create Embassy Repository

---
+++ Java Update/src/main/java/com/example/travelbuddy_api/repository/EmbassyRepository.java
@@ -0,0 +1,14 @@
+package com.example.travelbuddy_api.repository;
+
+import com.example.travelbuddy_api.model.Embassy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmbassyRepository extends JpaRepository<Embassy, Long> {
+
+    public List<Embassy> findAllByHomeCountry(String homeCountry);
+
+}
