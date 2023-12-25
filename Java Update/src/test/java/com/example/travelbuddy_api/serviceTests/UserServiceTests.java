31c6252120b759fadb458c86167d0bf583b22041
GabrielleHandy <gehandy123@gmail.com>
2023-12-24 22:11:20 -0500

    Create UserServiceTests

---
+++ Java Update/src/test/java/com/example/travelbuddy_api/serviceTests/UserServiceTests.java
@@ -0,0 +1,11 @@
+package com.example.travelbuddy_api.serviceTests;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+}
